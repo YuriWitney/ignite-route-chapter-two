82c4b7129f13c21e4c3f93f37746264bd5287ec4
Barros, Yuri <2165225@cognizant.com>
2022-10-18 17:06:09 -0300

    feat: add multer

--- src/routes/categories-routes.ts
+++ src/routes/categories-routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
+import multer from 'multer'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoryController } from '../modules/cars/useCases/listCategory'
 
 export const categoryRoutes = Router()
+const upload = multer({
+  dest: './tmp'
+})
 
 categoryRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response)
@@ -10,4 +14,10 @@ categoryRoutes.post('/', (request, response) => {
 
 categoryRoutes.get('/', (request, response) => {
   return listCategoryController.handle(request, response)
+})
+
+categoryRoutes.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request
+  console.log(file)
+  return response.send()
 })
