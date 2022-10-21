3f253bde7ca5228c3490be44d7866f6866601e0c
Barros, Yuri <2165225@cognizant.com>
2022-10-21 13:48:09 -0300

    feat: add swagger ui

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { router } from './routes'
-import { categoryRoutes } from './routes/categories-routes'
-import { specificationsRouter } from './routes/specifications-routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router)
 
 app.listen(3333, () => console.log('server is running on http://localhost:3333'))
