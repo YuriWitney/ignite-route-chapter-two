b3ef94811168e0039f257823b06c2efe125cbb06
Barros, Yuri <2165225@cognizant.com>
2023-06-07 09:39:40 -0300

    feat: move block code to a config file

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,4 @@
-import { DataSource } from 'typeorm'
-
-const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'docker',
-  password: 'ignite',
-  database: 'rentx'
-})
+import { AppDataSource } from '../../ormconfig'
 
 AppDataSource.initialize()
   .then(() => {
